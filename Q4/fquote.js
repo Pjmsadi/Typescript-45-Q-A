56181927d211cfaab3a1e7caa494b20a229820de
Sadia Batool <eshaalZahrah5@gmail.com>
2024-04-11 23:10:25 +0500

    first commit

---
+++ Q4/fquote.js
@@ -0,0 +1,3 @@
+var famousquote = "Never give up on a dream that you've been chasing almost your whole life.";
+var authorname = "Park Jimin";
+console.log("".concat(authorname, " once said,\"").concat(famousquote));
---
+++ Q4/fquote.ts
@@ -0,0 +1,4 @@
+let famousquote: string = "Never give up on a dream that you've been chasing almost your whole life.";
+let authorname: string= "Park Jimin";
+
+console.log(`${authorname} once said,"${famousquote}`);
